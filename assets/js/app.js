2237477a67ed33a3e9726e6f71a0ed49b9ce615b
christianrochford <rochfordchristian@gmail.com>
2014-01-22 21:32:01 +0000

    add parallax backgrounds

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,5 +8,20 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-	$('#intro').css({height:$(window).height()-46 + 'px'});
+	$('#intro, #about, #research, #questions').css({height:$(window).height()-46 + 'px'});
+});
+
+$(document).ready(function(){
+	$('#intro-arrow-down').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({scrollTop: $('#about').offset().top -46}, 500);
+	})
+	$('#about-arrow-down').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({scrollTop: $('#research').offset().top -46}, 500);
+	})
+	$('#research-arrow-down').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({scrollTop: $('#questions').offset().top -46}, 500);
+	})
 });
