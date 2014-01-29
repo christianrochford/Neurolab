7a7508f473d7f90194d7197245952a8ee873d3e8
christianrochford <rochfordchristian@gmail.com>
2014-01-29 23:18:29 +0000

    update menu and anchor links

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
+	if($(window).width() <= 767){
+		$('#to-about').on('click', function(e){
+			e.preventDefault();
+			// var stateObj1 = { foo: "about" };
+			// var newTitle1 = "about";
+			// var newUrl1 = "about";
+			// history.pushState(stateObj1, newTitle1, newUrl1);
+			$('html, body').animate({scrollTop: $('#about').offset().top -46}, 500);
+			$('nav').fadeOut(300);
+			$('#menu').removeClass('on');
+			$('#menu-close').hide();
+		})
+		$('#to-research').on('click', function(e){
+			e.preventDefault();
+			// var stateObj2 = { foo: "research" };
+			// var newTitle2 = "research";
+			// var newUrl2 = "research";
+			// history.pushState(stateObj2, newTitle2, newUrl2);
+			$('html, body').animate({scrollTop: $('#research').offset().top -46}, 500);
+			$('nav').fadeOut(300);
+			$('#menu').removeClass('on');
+			$('#menu-close').hide();
+		})
+	} else {
+		$('#to-about').on('click', function(e){
+			$('html, body').animate({scrollTop: $('#about').offset().top -46}, 500);
+		})
+		$('#to-research').on('click', function(e){
+			$('html, body').animate({scrollTop: $('#research').offset().top -46}, 500);
+		})
+	}
 	$('#menu').on('click', function(e){
 		e.preventDefault();
 		$(this).addClass('on');
@@ -11,24 +42,11 @@ $(document).ready(function(){
 		$('nav').fadeOut(300);
 		$(this).hide();
 	})
-	$('#to-about').on('click', function(e){
-		e.preventDefault();
-		$('html, body').animate({scrollTop: $('#about').offset().top -46}, 500);
-		$('nav').fadeOut(300);
-		$('#menu').removeClass('on');
-		$('#menu-close').hide();
-	})
-	$('#to-research').on('click', function(e){
-		e.preventDefault();
-		$('html, body').animate({scrollTop: $('#research').offset().top -46}, 500);
-		$('nav').fadeOut(300);
-		$('#menu').removeClass('on');
-		$('#menu-close').hide();
-	})
 });
 
 $(document).ready(function(){
-	$('#intro, #about, #about-content, #research, #research-content, #questions').css({height:$(window).height()-46 + 'px'});
+	$('#intro').css({height:$(window).height()-46 + 'px'});
+	$('#about, #about-content, #research, #research-content, #questions').css({height:$(window).height()-46});
 	if($(window).width() > 767){
 		skrollr.init({
                 forceHeight: false
@@ -36,9 +54,59 @@ $(document).ready(function(){
 	}
 });
 
+$(window).resize(function(){
+	if($(window).width() > 767){
+		$('nav').show();
+		skrollr.init({
+                forceHeight: false
+        });
+        $('#to-about').on('click', function(e){
+			e.preventDefault();
+			$('html, body').animate({scrollTop: $('#about').offset().top -46}, 500);
+			$('nav').show();
+			$('#menu').removeClass('on');
+			$('#menu-close').hide();
+		})
+		$('#to-research').on('click', function(e){
+			e.preventDefault();
+			$('html, body').animate({scrollTop: $('#research').offset().top -46}, 500);
+			$('nav').show();
+			$('#menu').removeClass('on');
+			$('#menu-close').hide();
+		})
+	} else {
+		$('nav').hide();
+		$('#to-about').on('click', function(e){
+			e.preventDefault();
+			$('html, body').animate({scrollTop: $('#about').offset().top -46}, 500);
+			$('nav').fadeOut(300);
+			$('#menu').removeClass('on');
+			$('#menu-close').hide();
+		})
+		$('#to-research').on('click', function(e){
+			e.preventDefault();
+			$('html, body').animate({scrollTop: $('#research').offset().top -46}, 500);
+			$('nav').fadeOut(300);
+			$('#menu').removeClass('on');
+			$('#menu-close').hide();
+		})
+	}
+})
+
 $(document).ready(function(){
-	$('#intro-arrow-down').on('click', function(e){
-		e.preventDefault();
+	$('#intro-arrow-down').on('click', function(){
 		$('html, body').animate({scrollTop: $('#about').offset().top -46}, 500);
+	})
+	$('#about-arrow-down').on('click', function(){
+		$('html, body').animate({scrollTop: $('#about-content').offset().top -46}, 500);
+	})
+	$('#about-content-arrow-down').on('click', function(){
+		$('html, body').animate({scrollTop: $('#research').offset().top -46}, 500);
+	})
+	$('#research-arrow-down').on('click', function(){
+		$('html, body').animate({scrollTop: $('#research-content').offset().top -46}, 500);
+	})
+	$('#research-content-arrow-down').on('click', function(){
+		$('html, body').animate({scrollTop: $('#questions').offset().top -46}, 500);
 	})
 });
